8862e83a95d3c669a9ed27cee4193615d8edaf2b
susumOyaji <sumitomo0210@gmail.com>
2021-07-17 11:24:32 +0900

    up

--- lib/untitled3.dart
+++ lib/untitled3.dart
@@ -3,11 +3,21 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Untitled3 {
-  static const MethodChannel _channel =
-      const MethodChannel('untitled3');
+  static const MethodChannel _channel = const MethodChannel('untitled3');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<String> get platformBattery async {
+    final String level = await _channel.invokeMethod('getPlatformBattery');
+    return level;
+  }
+
+  static Future<String> get platformTelephony async {
+    final String telephonyInfo =
+        await _channel.invokeMethod('getTelephonyInfo');
+    return telephonyInfo;
   }
 }
